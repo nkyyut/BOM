c78ab6d3636f832edc047ef096aa50fa455eef02
Chalice <tatsuki-k-17e@stu.kbc.ac.jp>
2018-04-26 18:07:53 +0900

    [追加]初期セッティング

--- BOM/PlayerControle.cpp
+++ BOM/PlayerControle.cpp
@@ -1,4 +1,38 @@
-#include "PlayerCharacter.h"
+#include "PlayerControle.h"
+
+void PlayerStateInit()
+{	
+	const int STATE_INIT = 2;
+
+	for (int i = PLAYER1; i < PLAYER_LIMIT;)
+	{
+		if (i % 2 == 0)
+		{
+			PlayerState[i].x = 100;
+		}
+		else
+		{
+			PlayerState[i].x = 650;
+		}
+
+		if (i < 2)
+		{
+			PlayerState[i].y = 100;
+		}
+		else
+		{
+			PlayerState[i].y = 650;
+		}
+
+		PlayerState[i].w = 50;
+		PlayerState[i].h = 100;
+		PlayerState[i].BCount = STATE_INIT;
+		PlayerState[i].BPower = STATE_INIT;
+		PlayerState[i].PSpeed = STATE_INIT;
+		PlayerState[i].img = i;
+		PlayerState[i].Alive = true;
+	}
+}
 
 void PlayerControl()
 {
